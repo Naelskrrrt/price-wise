dc7705be12fe36bad58af160ac64be2b8ae549cb
LALASON Annael <ralalanael@gmail.com>
2024-05-27 16:49:25 +0300

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Pricewise",
+  description:
+    "Pricewise is a price comparison tool that allows you to compare prices from different retailers.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="max-w-10xl mx-auto">
+          <Navbar />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
